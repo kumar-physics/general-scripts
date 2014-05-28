06d30ff6e6c6667299d305abab79886841beb5bb
Kumaran Baskaran <kumar.physics@gmail.com>
2014-05-28 16:54:31 +0200

    some update

--- rscripts/uniprot.R
+++ rscripts/uniprot.R
@@ -4,5 +4,8 @@ library(ggplot2)
 library(reshape2)
 d<-read.table('uniportogrowth.dat',header=T)
 d <- melt(d, id.vars="Uniprot")
-ggplot(d, aes(x=Uniprot,y=value, col=variable,fill=variable)) + geom_line(aes(group=variable))+ylab('% of PDB having at least 10 homolos')
-
+plot1=ggplot(d, aes(x=Uniprot,y=value, col=variable,fill=variable)) + geom_line(aes(group=variable))+ylab('% of PDB having at least 10 homolos(or)% of EPPIC predictability')+theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5))
+plot1
+pdf('uniprotvseppic.pdf')
+plot1
+dev.off()
