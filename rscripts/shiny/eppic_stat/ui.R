5753c9d410f1360dbe0292dff1f084d002e5b9cf
Kumaran Baskaran <kumar.physics@gmail.com>
2014-07-04 12:37:17 +0200

    new files added

---
+++ rscripts/shiny/eppic_stat/ui.R
@@ -0,0 +1,38 @@
+library(shiny)
+
+# Define UI for random distribution application 
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Eppic Statistics"),
+  
+  # Sidebar with controls to select the random distribution type
+  # and number of observations to generate. Note the use of the
+  # br() element to introduce extra vertical spacing
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("dist", "Data to plot:",
+                   c("Geometry" = "gm",
+                     "Core Rim" = "cr",
+                     "Core Surface" = "cs",
+                     "Final" = "final")),
+      br(),
+      
+      sliderInput("n", 
+                  "Area bin size:", 
+                  value = 50,
+                  min = 10, 
+                  max = 1000)
+    ),
+    
+    # Show a tabset that includes a plot, summary, and table view
+    # of the generated distribution
+    mainPanel(
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Plot", plotOutput("plot")), 
+                  tabPanel("Summary", verbatimTextOutput("summary")), 
+                  tabPanel("Table", tableOutput("table"))
+      )
+    )
+  )
+))
